cd65cda7e47ad057c6a2a1de85d40f732f39ecbf
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-10-18 10:58:23 +0000

    Generate Food scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :foods
   get '/users', to: 'users#new'
 
   resources :recipes, only: [:index, :destroy, :new, :show]
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_100118) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_105437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price"
+    t.integer "quantity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -32,5 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_100118) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users"
   add_foreign_key "recipes", "users"
 end
