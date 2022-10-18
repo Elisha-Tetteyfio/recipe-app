60b61907e3b1f1850f9ebc00904df92ed3cb66a5
bruk19 <brukteshome21@gmail.com>
2022-10-18 17:32:43 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
+
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  root 'users#new'
+   
   resources :recipe_foods
   resources :foods
   get '/users', to: 'users#new'
 
   resources :recipes, only: [:index, :destroy, :new, :show]
   resources :users
-  root 'foods#new'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
