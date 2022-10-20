dd986d2cb215ddb7e5a1d2552edd7d8319df2f4b
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-10-20 17:31:48 +0000

    Update ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,12 +5,8 @@ class Ability
     user ||= User.new
     can :read, :all
 
-    if user.role == 'admin'
-      can :manage, :all
-    else
-      can :manage, Food, user_id: user.id
-      can :read, Recipe, public: true
-      can :manage, Recipe, user_id: user.id
-    end
+    can :manage, Food, user_id: user.id
+    can :read, Recipe, public: true
+    can :manage, Recipe, user_id: user.id
   end
 end
